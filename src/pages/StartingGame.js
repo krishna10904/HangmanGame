f707fd3a97e32892795e5604a2b33999422384b3
Krishna Verma <krishnaverma10904@gmail.com>
2025-12-24 23:41:27 +0530

    Update StartingGame.js

--- src/pages/StartingGame.js
+++ src/pages/StartingGame.js
@@ -1,5 +1,14 @@
-function StartGame() {
-return (
+import { Link } from "react-router-dom";
+import TextInputFormContainer from "../components/TextInputForm/TextInputFormContainer";
 
+function StartGame() {
+    return (
+        <>
+            <h1>Start Game</h1>
+            <TextInputFormContainer />
+            <Link to='/play'  className="text-blue-400">Play Game Link</Link>
+        </>
+    )
 }
-export default StartGame;   
+
+export default StartGame;
